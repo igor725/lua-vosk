074578cf3a2c128117670733274b6cae4a0599fc
igor725 <gvaldovigor@gmail.com>
2022-03-23 19:55:15 +0300

    Fix bash build script

--- build.sh
+++ build.sh
@@ -3,4 +3,13 @@ if [ "$CC" = "" ]; then
 	CC="gcc"
 fi
 
-$CC -pipe -fPIC -shared src/*.c -ldl -otest/vosk.so
+if pkg-config --exists luajit; then
+	LUA_FLAGS=$(pkg-config --libs --cflags luajit)
+elif pkg-config --exists lua; then
+	LUA_FLAGS=$(pkg-config --libs --cflags lua)
+else
+	echo "Lua not found!"
+	exit 1
+fi
+
+$CC -rdynamic -pipe -fPIC -shared src/*.c -Isrc $LUA_FLAGS -ldl -otest/vosk.so
