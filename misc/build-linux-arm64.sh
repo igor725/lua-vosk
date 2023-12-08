e04988d734037ce6233751818f142818acf8c91c
igor725 <gvaldovigor@gmail.com>
2023-12-08 11:56:49 +0300

    CI test

--- misc/build-linux-arm64.sh
+++ misc/build-linux-arm64.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 MAINDIR=$(dirname `readlink -f "$0"`)
-cd luajit
+cd "$MAINDIR/luajit"
 make clean
 make HOST_CC=gcc CROSS=aarch64-linux-gnu- TARGET_SYS=Linux -j$(nproc)
-cd ..
+cd "$MAINDIR"
 mkdir -p out/linux-arm64
 cd out/linux-arm64
 cmake "$MAINDIR/.." -DCMAKE_TOOLCHAIN_FILE="$MAINDIR/TC-linux-arm64.cmake" -DLUA_INCLUDE_DIR="$MAINDIR/luajit/src/" -DLUA_LIBRARIES="$MAINDIR/luajit/src/libluajit.so"
