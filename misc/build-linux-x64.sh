cf4917e732274b4ff3fa4b18be56c1c3f00246f4
igor725 <gvaldovigor@gmail.com>
2023-12-08 12:10:49 +0300

    Fukin fuk

--- misc/build-linux-x64.sh
+++ misc/build-linux-x64.sh
@@ -3,9 +3,8 @@ MAINDIR=$(dirname `readlink -f "$0"`)
 cd "$MAINDIR/luajit"
 make clean
 make HOST_CC=gcc CROSS=x86_64-linux-gnu- TARGET_SYS=Linux -j$(nproc)
-cd "$$MAINDIR"
+cd "$MAINDIR"
 mkdir -p out/linux-amd64
 cd out/linux-amd64
 cmake "$MAINDIR/.." -DCMAKE_TOOLCHAIN_FILE="$MAINDIR/TC-linux-x64.cmake" -DLUA_INCLUDE_DIR="$MAINDIR/luajit/src/" -DLUA_LIBRARIES="$MAINDIR/luajit/src/libluajit.so"
-pwd
 make -j$(nproc)
